8e87b03b3d6439943b43139f5a1816f50d83db01
David Buzatto <davidbuzatto@gmail.com>
2024-12-03 07:50:53 -0300

    dia 03

--- src/d03/Solver.java
+++ src/d03/Solver.java
@@ -1,6 +1,8 @@
 package d03;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import utils.Utils;
 
 /**
@@ -12,16 +14,65 @@ public class Solver {
     public static void main( String[] args ) throws Exception {
         
         //solveEasy();
-        //solveHard();
+        solveHard();
         
     }
     
     private static void solveEasy() throws Exception {
+        
         Scanner scan = Utils.getScannerEasy( Solver.class );
+        StringBuilder sb = new StringBuilder();
+        while ( scan.hasNextLine() ) {
+            sb.append( scan.nextLine() );
+        }
+        
+        Pattern p = Pattern.compile( "(mul\\(\\d+,\\d+\\))" );
+        Matcher m = p.matcher( sb.toString() );
+        int sum = 0;
+        
+        while ( m.find() ) {
+            String e = m.group(1);
+            Pattern pe = Pattern.compile( "(\\d+),(\\d+)" );
+            Matcher me = pe.matcher( e );
+            while ( me.find() ) {
+                int n1 = Integer.parseInt( me.group(1) );
+                int n2 = Integer.parseInt( me.group(2) );
+                int mult = n1 * n2;
+                sum += mult;
+            }
+        }
+        
+        System.out.println( sum );
+        
     }
     
     private static void solveHard() throws Exception {
+        
         Scanner scan = Utils.getScannerHard( Solver.class );
+        
+        StringBuilder sb = new StringBuilder();
+        while ( scan.hasNextLine() ) {
+            sb.append( scan.nextLine() );
+        }
+        
+        Pattern p = Pattern.compile( "(mul\\(\\d+,\\d+\\))" );
+        Matcher m = p.matcher( sb.toString() );
+        int sum = 0;
+        
+        while ( m.find() ) {
+            String e = m.group(1);
+            Pattern pe = Pattern.compile( "(\\d+),(\\d+)" );
+            Matcher me = pe.matcher( e );
+            while ( me.find() ) {
+                int n1 = Integer.parseInt( me.group(1) );
+                int n2 = Integer.parseInt( me.group(2) );
+                int mult = n1 * n2;
+                sum += mult;
+            }
+        }
+        
+        System.out.println( sum );
+        
     }
     
 }
